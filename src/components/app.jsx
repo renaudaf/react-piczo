2ee8ea5eb34cf828e0258ac3e563579cf4b3e6cc
renaud archer fortin <renaud.archerf@gmail.com>
2021-01-18 17:09:04 -0500

    added router

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { Switch, Route } from "react-router-dom";
 import Header from "./header";
+import Cart from "../pages/cart";
+import Photos from "../pages/photos";
 
 function App() {
   return (
     <div className="container">
       <Header />
-      <h1>Home Page</h1>
+      <Switch>
+        <Route exact path="/"><Photos /></Route>
+        <Route path="/cart"><Cart /></Route>
+      </Switch>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 import '../assets/stylesheets/application.scss';
 import App from "./components/app";
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<App />, root);
+  ReactDOM.render(<Router><App /></Router>, root);
 }
