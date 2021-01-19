486f90036b18a5d29a38413e5ba950f3790a12e3
renaud archer fortin <renaud.archerf@gmail.com>
2021-01-19 12:44:56 -0500

    cart item added

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import PropTypes from "prop-types";
+import { Context } from "../context";
 
 const Image = ({ className, img }) => {
+  const [hovered, setHovered] = useState(false);
+  const { toggleFavorite, addToCart } = useContext(Context);
+  const heartIcon = () => {
+    if (img.isFavorite) {
+      return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)} />;
+    } if (hovered) {
+      return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)} />;
+    }
+  };
+
+  const cartIcon = hovered && <i className="ri-add-circle-line cart" onClick={() => addToCart(img)} />;
   return (
-    <div className={className}>
-      <img src={img.url} className="image-grid" alt="wow" />
+    <div
+      className={`${className} image-container`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <img src={img.url} className="image-grid" alt="piczo" />
+      {heartIcon()}
+      {cartIcon}
     </div>
   );
+};
+
+Image.propTypes = {
+  className: PropTypes.string,
+  img: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool
+  })
 };
 
 export default Image;
--- src/context.jsx
+++ src/context.jsx
@@ -5,10 +5,26 @@ import pictures from "../data/pictures";
 const Context = React.createContext();
 
 const ContextProvider = ({ children }) => {
-  const [allPhotos] = useState(pictures);
+  const [allPhotos, setAllPhotos] = useState(pictures);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (newImage) => {
+    setCartItems(prevImages => [...prevImages, newImage]);
+  };
+  console.log(cartItems);
+
+  const toggleFavorite = (id) => {
+    const newArr = allPhotos.map((photo) => {
+      if (photo.id === id) {
+        return { ...photo, isFavorite: !photo.isFavorite };
+      }
+      return photo;
+    });
+    setAllPhotos(newArr);
+  };
 
   return (
-    <Context.Provider value={{ allPhotos }}>
+    <Context.Provider value={{ allPhotos, toggleFavorite, addToCart }}>
       {children}
     </Context.Provider>
   );
